94c0809c1d820c2f792f4821452b418c6ad79fb7
Parit Kansal <132123353+ParitKansal@users.noreply.github.com>
2026-08-21 07:30:36 +0530

    Solve pandas-loc-iloc

---
+++ study-plans/pandas-basics/pandas-loc-iloc/pandas-loc-iloc.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def iloc_selection(data, row, col):
+    """
+    Returns: list [element, row_values, col_values]
+    """
+    df = pd.DataFrame(data)
+    return [
+        df.iloc[row, col],
+        list(df.iloc[row]),
+        list(df.iloc[:,col])
+    ]
