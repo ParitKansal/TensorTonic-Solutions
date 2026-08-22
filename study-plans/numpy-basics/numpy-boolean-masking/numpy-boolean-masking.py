bf0ff3a40a02fed4eb1114d8e6397f14350bf9c2
Parit Kansal <132123353+ParitKansal@users.noreply.github.com>
2026-08-22 13:23:01 +0530

    Solve numpy-boolean-masking

---
+++ study-plans/numpy-basics/numpy-boolean-masking/numpy-boolean-masking.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def row_summary(data, threshold):
+    """Returns: np.ndarray of shape (3, m, n), stacked element mask, any-filtered, all-filtered"""
+    data = np.array(data, dtype="float64")
+    return np.array([np.array(data>threshold, dtype = "float64"),
+                     [x if np.any(x > threshold) else np.zeros(x.shape) for x in data],
+                     [x if np.all(x > threshold) else np.zeros(x.shape) for x in data]
+                    ])
