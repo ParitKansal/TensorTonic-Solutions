7791b32429ab9d8f91723fe960bde9cae54b7ff3
Parit Kansal <132123353+ParitKansal@users.noreply.github.com>
2026-08-22 18:45:11 +0530

    Solve numpy-concat-correlate

--- study-plans/numpy-basics/numpy-concat-correlate/numpy-concat-correlate.py
+++ study-plans/numpy-basics/numpy-concat-correlate/numpy-concat-correlate.py
@@ -1,10 +1,15 @@
 import numpy as np
-
+def fun(A):
+    X = A - A.mean(axis=0)
+    C = X.T @ X / (len(X) - 1)
+    std = np.sqrt(np.diag(C))
+    std_ = std[:, None]@std[None, :]
+    return C/std_
 def compare_correlations(a, b):
     """Returns: np.ndarray of shape (3, n, n), stacked correlation matrices"""
     return np.array([
-        np.corrcoef(np.array(a).T),
-        np.corrcoef(np.array(b).T),
-        np.corrcoef(np.array(a+b).T)
+        fun(np.array(a)),
+        fun(np.array(b)),
+        fun(np.array(a+b))
     ]
     )
