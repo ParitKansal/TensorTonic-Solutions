3c76cb0b1a093554e45abb263fe10af85473d414
Parit Kansal <132123353+ParitKansal@users.noreply.github.com>
2026-08-22 15:57:27 +0530

    Solve numpy-aggregation

---
+++ study-plans/numpy-basics/numpy-aggregation/numpy-aggregation.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def summarize(data, axis):
+    """Returns: np.ndarray of shape (4, k), rows are mean, std, min, max"""   
+    data = np.array(data)
+    return [
+        np.mean(data, axis = axis),
+        np.std(data, axis = axis),
+        np.min(data, axis = axis),
+        np.max(data, axis = axis) 
+    ]
+    pass
