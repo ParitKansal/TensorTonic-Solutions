4b4ed23abd382b870b33183dcfd707203f780ac6
Parit Kansal <132123353+ParitKansal@users.noreply.github.com>
2026-08-22 15:26:05 +0530

    Solve numpy-angle-features

---
+++ study-plans/numpy-basics/numpy-angle-features/numpy-angle-features.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def angle_features(angles):
+    """Returns: np.ndarray of shape (3, n), rows are sin, cos, tan"""
+    return np.array(
+        [
+            np.sin(np.array(angles)), 
+            np.cos(np.array(angles)), 
+            np.tan(np.array(angles))
+        ]
+    )
