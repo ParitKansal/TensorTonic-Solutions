5b33b3460bffeb94bedb6b0657e80448421093b7
Parit Kansal <132123353+ParitKansal@users.noreply.github.com>
2026-08-23 17:34:11 +0530

    Solve gelu

--- study-plans/cuda-basics/gelu/gelu.cu
+++ study-plans/cuda-basics/gelu/gelu.cu
@@ -1,20 +1,33 @@
 #include <cuda_runtime.h>
 #include <math.h>
 
-__global__ void gelu_kernel(const float* input, float* output, int N) {
-    // Write code here
+__global__ void k_fun(float* k) {
+    if (blockIdx.x == 0 && threadIdx.x == 0) {
+        *k = sqrtf(2.0f / 3.14159265358979323846f);
+    }
+}
+
+__global__ void gelu_kernel(const float* input, float* output, const float* k, int N) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if(idx < N){
+
+    if (idx < N) {
         float x = input[idx];
-        output[idx] = 0.5f * x *(1.0f + tanhf(
-            0.7978845608f *(x + 0.044715f * x*x*x)
-        ));
+        output[idx] = 0.5f * x *
+            (1.0f + tanhf((*k) * (x + 0.044715f * x * x * x)));
     }
 }
 
 extern "C" void solve(const float* input, float* output, int N) {
     int threads = 256;
-    dim3 blocks((N + 255) / 256);
-    gelu_kernel<<<blocks, threads>>>(input, output, N);
+    int blocks = (N + threads - 1) / threads;
+
+    float* d_k;
+    cudaMalloc(&d_k, sizeof(float));
+
+    k_fun<<<1, 1>>>(d_k);
+    gelu_kernel<<<blocks, threads>>>(input, output, d_k, N);
+
     cudaDeviceSynchronize();
+
+    cudaFree(d_k);
 }
